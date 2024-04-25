a93bccc72b3cf81f65d13a02a49f201d38f602b2
Muhammad Ikhsan <muh.ikhsan818@gmail.com>
2024-04-25 12:15:38 +0800

    fix: add Home.jsx deleted

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="container mx-auto border border-red-600">
+        <h2>This is Home Page</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
